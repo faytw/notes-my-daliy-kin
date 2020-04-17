9bd379b73c18a6f18e72ae51e08c356718a9c54a
Fay <rebecca.hsu1989@gmail.com>
2020-04-17 14:35:28 +0800

    add style to mark the square that match hours

--- src/components/Today.vue
+++ src/components/Today.vue
@@ -4,25 +4,21 @@
       <v-col class="mb-1">
         <div class="font-weight-bold">{{ displayDate }}</div>
       </v-col>
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <v-row justify="center">
-          <v-col 
-            class='mc-container col-md-4'
-            cols="8">
-            <div 
-              class="square" 
-              v-for='item in custom' 
-              :key='"kin" + item.kin' 
-              :data-mc='item.location'
-            >
-              <v-img :src='require(`../assets/${item.icon}.png`)'/>
-              <div>KIN {{ item.kin }}</div>
-            </div>
-          </v-col>
-        </v-row>
+    </v-row>
+    <v-row justify="center">
+      <v-col 
+        class='mc-container col-md-4'
+        cols="8">
+        <div 
+          class="square" 
+          v-for='item in custom' 
+          :key='"kin" + item.kin' 
+          :data-mc='item.location'
+          :data-now='item.now'
+        >
+          <v-img :src='require(`../assets/${item.icon}.png`)'/>
+          <div class="text-center">KIN {{ item.kin }}</div>
+        </div>
       </v-col>
     </v-row>
   </v-container>
@@ -39,19 +35,61 @@ const custom = [
 export default {
   name: 'today',
   data: () => ({
-    displayDate: '',
+    today: {},
     custom: custom
   }),
   mounted(){
-    const d = new Date()
-    const year = d.getFullYear()
-    const month = d.getMonth()
-    const date = d.getDate()
-    const hour = d.getHours()
-    const minute = d.getMinutes()
+    this.setDate()
+    this.setDataAt()
+  },
+  computed: {
+    displayDate() {
+      const { year, month, date } = this.today
+      return `${year} / ${month} / ${date}`
+    }
+  },
+  methods: {
+    setDate(input = new Date()) {
+      const d = input
+      const year = d.getFullYear()
+      const month = d.getMonth()
+      const date = d.getDate()
+      const hours = d.getHours()
+      const minutes = d.getMinutes()
 
-    this.displayDate = `${year} / ${month} / ${date}`
-    this.displayTime = `${hour} : ${minute}`
+      this.today = {
+        year,
+        month,
+        date,
+        hours,
+        minutes
+      }
+    },
+    setDataAt(input = this.today.hours) {
+      const list = [
+        { at: 't1', val: [0, 1, 2, 3, 4, 5] },
+        { at: 't2', val: [6, 7, 8, 9, 10, 11] },
+        { at: 't3', val: [12,13,14,15,16,17] },
+        { at: 't4', val: [18,19,20,21,22,23] }
+      ]
+      const table = {
+        t1: 'right', t2: 'top', t3: 'left', t4: 'bottom'
+      }
+      const temp = this.custom.slice()
+
+      list.forEach((item) => {
+        if (item.val.includes(input)) {
+          temp.forEach((elm ,index) => {
+            if (elm.location === table[item.at]) {
+              elm = { ...elm,
+                now: true
+              }
+              this.custom.splice(index, 1, elm)
+            }
+          })
+        }     
+      })
+    }
   }
 }
 </script>
@@ -62,6 +100,7 @@ export default {
     width: 30%;
     display: inline-block;
     margin: 0px 3px;
+    padding: 3px 5px;
   }
   .square[data-mc='top'] {
     display: block;
@@ -72,6 +111,10 @@ export default {
     display: block;
     margin: 0 auto;
     margin-top: 5px
+  }
+  .square[data-now] {
+    box-shadow: 3px 3px 3px cadetblue;
+    border-radius: 8px;
   }
   .display-time {
     font-size: 0.375em;
